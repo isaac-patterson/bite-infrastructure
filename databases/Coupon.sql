ce124f4f3768f8ec306e9fc83a917d179c876442
Isaac P <nzipatterson@gmail.com>
2021-09-02 22:13:07 +1200

    Added more coupon sample data

--- databases/Coupon.sql
+++ databases/Coupon.sql
@@ -1,2 +1,6 @@
-INSERT INTO Coupon (CreatedDate, ExpiryDate, Discount, DiscountCode)
-VALUES (CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 0.5, "TEST COUPON")
+INSERT INTO Coupon (RestaurantId, CreatedDate, ExpiryDate, Discount, DiscountCode)
+VALUES ("0832b802-9892-4964-a645-2fa3a3a0f898", CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 0.5, "TEST COUPON")
+
+INSERT INTO Coupon (RestaurantId, CreatedDate, ExpiryDate, Discount, DiscountCode)
+VALUES ("0832b802-9892-4964-a645-2fa3a3a0f898", CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 0.1, "test coupon 2")
+
